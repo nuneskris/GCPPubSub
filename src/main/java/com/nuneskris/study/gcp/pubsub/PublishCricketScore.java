df1715b93d07a1ace538b4203a7bd45dbab07423
krisnunes <Grace@1982>
2022-01-23 20:15:19 -0800

    CricketScore Avro

--- src/main/java/com/nuneskris/study/gcp/pubsub/PublishCricketScore.java
+++ src/main/java/com/nuneskris/study/gcp/pubsub/PublishCricketScore.java
@@ -1,6 +1,9 @@
 package com.nuneskris.study.gcp.pubsub;
 
 import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutureCallback;
+import com.google.api.core.ApiFutures;
+import com.google.api.gax.rpc.ApiException;
 import com.google.cloud.pubsub.v1.Publisher;
 import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.PubsubMessage;
@@ -16,6 +19,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import com.google.common.util.concurrent.MoreExecutors;
 
 public class PublishCricketScore {
 
@@ -57,7 +61,8 @@ public class PublishCricketScore {
             try (CSVReader reader = new CSVReader(new FileReader("src/main/resources/IPLBall-by-Ball 2008-2020.csv"))) {
                 String[] lineInArray;
                 int limitTesting = 0;
-                while ((lineInArray = reader.readNext()) != null || limitTesting++ < 10) {
+                while ((lineInArray = reader.readNext()) != null || limitTesting < 10) {
+                    limitTesting = limitTesting + 1;
                     CricketScore score = CricketScore.newBuilder()
                             .setId(lineInArray[0])
                             .setInning(Integer.valueOf(lineInArray[1]))
@@ -86,7 +91,31 @@ public class PublishCricketScore {
                     // Publish the encoded object as a Pub/Sub message.
                     ByteString data = ByteString.copyFrom(byteStream.toByteArray());
                     PubsubMessage message = PubsubMessage.newBuilder().setData(data).build();
+
                     ApiFuture<String> future = publisher.publish(message);
+                    // Add an asynchronous callback to handle success / failure
+                    ApiFutures.addCallback(
+                            future,
+                            new ApiFutureCallback<String>() {
+
+                                @Override
+                                public void onFailure(Throwable throwable) {
+                                    if (throwable instanceof ApiException) {
+                                        ApiException apiException = ((ApiException) throwable);
+                                        // details on the API exception
+                                        System.out.println(apiException.getStatusCode().getCode());
+                                        System.out.println(apiException.isRetryable());
+                                    }
+                                    System.out.println("Error publishing message : " + message);
+                                }
+
+                                @Override
+                                public void onSuccess(String messageId) {
+                                    // Once published, returns server-assigned message ids (unique within the topic)
+                                    System.out.println("Published message ID: " + messageId);
+                                }
+                            },
+                            MoreExecutors.directExecutor());
                 }
             }
         } finally {
